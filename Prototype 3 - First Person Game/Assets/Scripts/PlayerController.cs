13228e20c59a17108090688adf1fa773de0dc952
Damon <10837771@uvu.edu>
2021-10-07 02:07:12 -0600

    Player movement and mouse movement - First Person Game

--- Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        camera = camera.main;
+        camera = Camera.main;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -29,20 +29,42 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        CamLook();
+
+        if(Input.GetButtonDown("Jump"))
+            Jump();
     }
-void Move()
+    void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
 
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+        // rb.velocity = new Vector3(x, rb.velocity.y, z); <- Old code
+        Vector3 dir = transform.right * x + transform.forward * z;
+        rb.velocity = dir;
+
+
     }
 
     void CamLook()
     {
         float y = Input.GetAxis("Mouse X") * lookSensitivity;
         rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+        camera.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
     }
+
+    void Jump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if(Physics.Raycast(ray, 1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+
 }
 
 
